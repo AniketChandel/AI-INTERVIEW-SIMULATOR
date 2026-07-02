7139232be0b0d84b5b6ca20398516499952e3e9c
John Doe <john.doe@example.com>
2026-07-02 23:45:28 +0530

    Add server app.js

--- server/app.js
+++ server/app.js
@@ -32,24 +32,15 @@ app.post("/upload", upload.single("resume"), async (req, res) => {
 
     const data = fs.readFileSync(req.file.path);
     const pdf = await pdfParse(data);
-    const restext = pdf.text;    
+    
+    const resptext =  pdf.text.slice(0, 6000);   
 const question = await api.models.generateContent({
   model: "gemini-2.5-flash",
-  contents: `
-You are an experienced technical interviewer.
+contents: `
+Resume:
+${resptext}
 
-Candidate Resume:
-${restext}
-
-Tasks:
-1. Analyze the resume.
-2. Give an ATS score out of 100 based on the resume only but it should not reach above 95.
-3. Extract all technical skills.
-4. Ask ONE medium-difficulty interview question based on the resume.
-5. Do NOT give the answer.
-6. Return ONLY valid JSON.
-
-JSON format:
+Analyze the resume and return ONLY this JSON:
 
 {
   "resumescore": 0,
@@ -64,26 +55,30 @@ JSON format:
   "question": {
     "questionnumber": 1,
     "question": "",
-    "difficulty": "Medium",
+    "difficulty": "medium",
     "skill": "",
     "interviewComplete": false
   }
 }
-  Return ONLY a raw JSON object.
 
-Do not wrap the JSON inside ``json or ``.
-
-Do not add any explanation before or after the JSON.
-
+Requirements:
+- ATS score must be between 0 and 95.
+- Extract all technical skills into the correct categories.
+- Generate one medium interview question based on the candidate's skills and projects.
+- Do not provide the answer.
+- Return only valid JSON.
 `
 })
+const cleaned = question.text
+  .replace(/```json/g, "")
+  .replace(/```/g, "")
+  .trim();
 
+res.json(JSON.parse(cleaned));
 
     fs.unlinkSync(req.file.path);
 
-    res.json(
-    JSON.parse(question.text)
-    );
+ 
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -91,6 +86,46 @@ Do not add any explanation before or after the JSON.
     });
   }
 });
+app.post("/checkanswer", async (req, res) => {
+  try {
+    const { question, answer } = req.body;
+
+    const response = await api.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: `
+You are an interview evaluator.
+
+Interview Question:
+${question}
+
+Candidate Answer:
+${answer}
+
+Evaluate the candidate's answer.
+
+Give:
+1. Whether the answer is correct or not.
+2. Score out of 10.
+3. A short explanation.
+4. The ideal answer.
+
+Return only plain text.
+`,
+    });
+
+    res.json({
+      feedback: response.text,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      feedback: "Something went wrong."
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
